cc4cc553034c3c8dec0dfe6969147c963361995a
Angelo Netho <angelonetho@gmail.com>
2024-11-23 02:32:51 -0300

    feat: Update Client to handle server chat room

---
+++ src/Client/Client.java
@@ -0,0 +1,48 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private static final int SERVER_PORT = 22201;
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", SERVER_PORT);
+            System.out.println("✅ Connected to " + socket.getRemoteSocketAddress());
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println(in.readLine());
+
+            new Thread(() -> {
+                try {
+                    String serverMessage;
+                    while ((serverMessage = in.readLine()) != null) {
+                        System.out.println(serverMessage);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error: " + e.getMessage());
+                    System.exit(1);
+                }
+            }).start();
+
+            String userMessage;
+            while ((userMessage = console.readLine()) != null) {
+                out.println(userMessage);
+            }
+
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+
+        }
+    }
+}
