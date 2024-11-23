1bc1b5fe4d944e6f76f5f02f7546b7e008fd86b4
Angelo Netho <angelonetho@gmail.com>
2024-11-22 23:41:42 -0300

    feat: Implement Server

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(2201);
+        System.out.println("Server running on port 2201");
+
+        Socket clientSocket = server.accept();
+        System.out.println("Client connected");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        String message = in.readLine();
+        System.out.println("Client: " + message);
+
+        out.println("Server received: " + message);
+
+        clientSocket.close();
+        server.close();
+    }
 }
