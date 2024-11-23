2aeadc0cbc5ecd0be80c633f0a0a69b4058955bd
Angelo Netho <angelonetho@gmail.com>
2024-11-22 23:41:49 -0300

    feat: Implement Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 2201);
+
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        out.println("teste");
+
+        String response = in.readLine();
+        System.out.println(response);
+
+        socket.close();
+    }
 }
